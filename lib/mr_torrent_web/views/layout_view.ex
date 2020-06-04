a94b85093487d2c263b2a7c69a71f7c76692ba67
Andreas Alin <andreas.alin@gmail.com>
2020-06-04 07:59:55 -0500

    Remove some markup

--- lib/mr_torrent_web/views/layout_view.ex
+++ lib/mr_torrent_web/views/layout_view.ex
@@ -1,3 +1,9 @@
 defmodule MrTorrentWeb.LayoutView do
   use MrTorrentWeb, :view
+
+  @app_version Application.spec(:mr_torrent, :vsn) |> to_string
+
+  def app_version do
+    @app_version
+  end
 end
