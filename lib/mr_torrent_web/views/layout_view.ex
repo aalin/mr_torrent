ae40fbb3aae9511326234090ab3ce7f69ad910c4
Andreas Alin <andreas.alin@gmail.com>
2020-06-06 14:41:22 -0500

    Fix app version in footer

--- lib/mr_torrent_web/views/layout_view.ex
+++ lib/mr_torrent_web/views/layout_view.ex
@@ -1,9 +1,7 @@
 defmodule MrTorrentWeb.LayoutView do
   use MrTorrentWeb, :view
 
-  @app_version Application.spec(:mr_torrent, :vsn) |> to_string
-
   def app_version do
-    @app_version
+    Application.spec(:mr_torrent, :vsn)
   end
 end
