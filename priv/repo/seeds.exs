9a1e7af86c7c22d93b2fe9669b22fae258330a2b
Andreas Alin <andreas.alin@gmail.com>
2020-06-08 23:19:37 -0500

    Add some categories from archive.org to seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,6 +10,20 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-IO.puts "Creating default category"
+IO.puts "Creating movie categories"
 
-MrTorrent.Repo.insert!(%MrTorrent.Torrents.Category{name: "Default category"})
+movies = MrTorrent.Repo.insert!(%MrTorrent.Torrents.Category{name: "Movies"})
+
+["Feature films", "Sci-fi / horror", "Comedy", "Silent movies", "Noir"]
+|> Enum.each(fn name ->
+  MrTorrent.Repo.insert!(%MrTorrent.Torrents.Category{name: name, parent_id: movies.id})
+end)
+
+IO.puts "Creating music categories"
+
+music = MrTorrent.Repo.insert!(%MrTorrent.Torrents.Category{name: "Music"})
+
+["Live concert", "Rock", "Grateful Dead", "Reggae", "Jazz", "Hiphop"]
+|> Enum.each(fn name ->
+  MrTorrent.Repo.insert!(%MrTorrent.Torrents.Category{name: name, parent_id: music.id})
+end)
