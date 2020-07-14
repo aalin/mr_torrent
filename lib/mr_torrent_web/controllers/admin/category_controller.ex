90d7e7c1a4b399a00adb6ec315f2b7c39ee5bee6
Andreas Alin <andreas.alin@gmail.com>
2020-07-14 12:38:51 -0500

    Prefix unused variable with underscore

--- lib/mr_torrent_web/controllers/admin/category_controller.ex
+++ lib/mr_torrent_web/controllers/admin/category_controller.ex
@@ -1,7 +1,7 @@
 defmodule MrTorrentWeb.Admin.CategoryController do
   use MrTorrentWeb, :controller
 
-  def index(conn, params) do
+  def index(conn, _params) do
     categories = MrTorrent.Torrents.list_categories
     render(conn, "index.html", categories: categories)
   end
